7cde83374a64fae9b38c97861385ac6e326e2d96
浅仓雨 <72779032+March-rain233@users.noreply.github.com>
2022-11-23 14:15:33 +0800

    storestate

---
+++ Assets/Script/State/StoreState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class StoreState : GameState
+{
+    protected internal override void OnEnter()
+    {
+
+    }
+
+    protected internal override void OnExit()
+    {
+
+    }
+
+    protected internal override void OnUpdata()
+    {
+
+    }
+}
