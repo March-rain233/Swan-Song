649c4b65f419b63250bc941300b69e09a9b1e9e4
peipeicode <98679540+peipei-code-station@users.noreply.github.com>
2022-12-19 23:45:01 +0800

    merge

---
+++ Assets/Script/Buff/Cage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Cage : RoundBuff
+{
+    //牢笼状态 被困在5x5的方格内无法出去，在里面可以移动
+}
