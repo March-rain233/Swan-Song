81db9ef184c9e5274ee5d34272ecbb4f28892b36
peipeicode <98679540+peipei-code-station@users.noreply.github.com>
2022-12-20 15:46:37 +0800

    Add files via upload

---
+++ Assets/Script/Buff/GainBoss.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class GainBoss: RoundBuff
+{
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        Unit.UnitData.AttackWrapper.Rate = 1.1f;
+        Unit.UnitData.Blood = (int)(Unit.UnitData.Blood *1.1f);
+    }
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        Unit.UnitData.AttackWrapper.Rate = 1;
+        Unit.UnitData.Blood = (int)(Unit.UnitData.Blood * 1);
+    }
+
+
+    public override BuffData GetBuffData()
+    {
+        var res = base.GetBuffData();
+        res.Name = "增益";
+        res.Description = "Boss增益";
+        return res;
+    }
+
+}
+
+
+
